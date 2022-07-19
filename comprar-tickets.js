f37134be45e161e2af7613ea1da491ce690fd708
Segovia-Nicolas <nicosegovia1985@gmail.com>
2022-07-19 13:05:23 -0300

    arreglo error en JS

--- comprar-tickets.js
+++ comprar-tickets.js
@@ -1,80 +1,110 @@
+// Defino valor de ticket
 const valorTicket = 200;
 
-let desEstudiante = 80;
-let desTrainee = 50;
-let desJunior = 15;
+// Defino porcentajes de descuento según categoría
+let descuentoEstudiante = 80;
+let descuentoTrainee    = 50;
+let descuentoJunior     = 15;
 
-let nombre = document.getElementById("nombre");
-let apellido = document.getElementById("apellido");
-let email = document.getElementById("correo");
-let cantidadTickets = document.getElementById("cantidad");
-let categoria = document.getElementById("categoria");
+// Elementos en variables
+let nombre          = document.getElementById("nombre");
+let apellido        = document.getElementById("apellido");
+let mail            = document.getElementById("mail");
+let cantidadTickets = document.getElementById("cantidadTickets");
+let categoria       = document.getElementById("categoriaSelect");
 
-function resumen(){
-    //Validaciones de los campos
-    if(nombre.value === ""){
-       alert("Por favor, ingrese su nombre");
-       nombre.focus();
-       return; 
+// Función para quitar el estilo de error a los elementos del form
+function quitarClaseError() {
+    let x = document.querySelectorAll(".form-control, .form-select");
+    let i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('is-invalid');
     }
-    if(apellido.value === ""){
-        alert("Por favor, ingrese su apellido");
+}
+
+// Cálculo total a pagar
+function total_a_pagar() {
+
+    // Ejecuto función para que quite todos los estilos de error en los campos que los tuvieran
+    quitarClaseError();
+
+    // Verifico si lleno los siguientes campos, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if (nombre.value === "") {
+        alert("Por favor, escribí tu nombre.");
+        nombre.classList.add("is-invalid");
+        nombre.focus();
+        return;
+    }
+
+    if (apellido.value === "" ) {
+        alert("Por favor, escribí tu apellido.");
+        apellido.classList.add("is-invalid");
         apellido.focus();
-        return; 
-    }
-    if(email.value === ""){
-        alert("Por favor, ingrese su email");
-        email.focus();
-        return; 
-    }
-    const validarEmail = email => {
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    }
-    
-    if(!validarEmail(email.value)){
-        alert("Por favor, ingrese un correo electrónico válido");
-        email.focus();
         return;
     }
-    if((cantTickets.value == 0) || (isNaN (cantTickets.value))){
-        alert("Ingrese una cantidad válida de tickets");
-        cantTickets.focus();
+
+    if (mail.value === "") {
+        alert("Por favor, escribí tu email.");
+        mail.classList.add("is-invalid");
+        mail.focus();
         return;
     }
-    if(categoria.value ==""){
-        alert("Por favor, seleccione una categoría");
+
+    // Para determinar si el correo electrónico es válido o no
+    const emailValido = mail => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+    }
+
+    if (!emailValido(mail.value)) {
+        alert("Por favor, escribí un correo electrónico válido.");
+        mail.classList.add("is-invalid");
+        mail.focus();
+        return;
+    }
+
+    // Verifico si está ingresado al menos 1 ticket, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if ( (cantidadTickets.value == 0) || (isNaN(cantidadTickets.value)) ) {
+        alert("Por favor, ingresá correctamente cantidad de tickets.");
+        cantidadTickets.classList.add("is-invalid");
+        cantidadTickets.focus();
+        return;
+    }
+
+    // Verifico que haya seleccionado una categoría, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if (categoria.value == "") {
+        alert("Por favor, seleccioná una categoría.");
+        categoria.classList.add("is-invalid");
         categoria.focus();
         return;
     }
 
-    //Multiplico la cantidad de tickets por el valor
-    
+    // Multiplico cantidad de tickets por el valor
     let totalValorTickets = (cantidadTickets.value) * valorTicket;
 
-    //Calculo el valor según categoría
-    if (categoria.value ==0) {
-        totalValorTickets = totalValorTickets;
+    // Aplico descuentos según categoría
+    if (categoria.value == 0) {
+        totalValorTickets = totalValorTickets ;
     }
-    if (categoria.value ==1) {
-        totalValorTickets = totalValorTickets - (desEstudiante / 100 * totalValorTickets);
+    if (categoria.value == 1) {
+        totalValorTickets = totalValorTickets - (descuentoEstudiante / 100 * totalValorTickets);
     }
-    if (categoria.value ==2) {
-        totalValorTickets = totalValorTickets - (desTrainee / 100 * totalValorTickets);
+    if (categoria.value == 2) {
+        totalValorTickets = totalValorTickets - (descuentoTrainee / 100 * totalValorTickets);
     }
-    if (categoria.value ==3) {
-        totalValorTickets = totalValorTickets - (desJunior / 100 * totalValorTickets);
+    if (categoria.value == 3) {
+        totalValorTickets = totalValorTickets - (descuentoJunior / 100 * totalValorTickets);
     }
-    //Le digo que me lo muestre en el html, como contenido de la etiqueta correspondiente
-    totalAPagar.innerHTML = "Total a pagar: $" + totalValorTickets;    
+
+    // Inserto el valor en el HTML
+    totalPago.innerHTML = "Total a pagar: $" + totalValorTickets;
 }
 
-//Llamo a la función y le digo que se ejecute cuando el usuario haga click en el botón Resumen
-btnResumen.addEventListener('click', resumen);
+// Botón Resumen recibe un escuchador y la funcion del cálculo
+btnResumen.addEventListener('click', total_a_pagar);
 
-//botón Borrar
-function reset_resumen (){
-    totalAPagar.innerHTML = "";
+// Función para el botón Borrar para que borre el valor
+function reset_total_a_pagar() {
+    quitarClaseError();
+    totalPago.innerHTML = "";
 }
-btnBorrar.addEventListener('click', reset_resumen);
-
-//El cálculo lo hace bien, pero enseguida da un salto y se resetea sin dar click al botón Borrar
+btnBorrar.addEventListener('click', reset_total_a_pagar);
